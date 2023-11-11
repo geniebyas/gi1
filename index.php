4638d5dde1bc54f88abf9febb979bf2535ed5827
PatelShakil <patelsakib95@gmail.com>
2023-11-11 21:28:23 +0530

    services

--- index.php
+++ index.php
@@ -158,32 +158,72 @@ include 'header.php'; ?>
 <div style="font-family: 'Poppins', Arial, sans-serif; text-align: center;">
     <h1 style="font-size: 40px; margin: 30px 0; font-weight: bold;">Our Services</h1>
 </div>
-<section class="blog-section">
-    <div class="blog-item">
-        <div class="blog-content">
-            <h2 class="blog-title">Top Industries</h2>
-            <div class="blog-description">
+
+<div class="col-12">
+    <style>
+        li {
+            text-align: left;
+        }
+    </style>
+    <div class="col-12 text-center">
+        <div class="card col-md-3 m-1">
+            <div class="card-header">
+                <h2>Top Platforms</h2>
+            </div>
+            <div class="card-body">
                 <ul>
-                    <li>Gi Media</li>
+                    <li>Gi1 Media</li>
+                    <li>Gi1 Mall</li>
+                    <li>Gi1 Video Hub</li>
+                    <li>Gi1 Edits</li>
+                    <li>Gi1 Game Zone</li>
+                    <li>Gi1 Beauty</li>
+                    <li>Gi1 Travels</li>
+                    <li>Gi1 Podcasts</li>
+                    <li>Gi1 Classes</li>
+                    <li>Gi1 Hiring</li>
+                </ul>
+            </div>
+        </div>
+        <div class="card col-md-3 m-1">
+            <div class="card-header">
+                <h2>Main Platforms</h2>
+            </div>
+            <div class="card-body">
+                <ul>
+                    <li>Gi1 Health</li>
+                    <li>Gi1 Memes</li>
+                    <li>Gi1 Trading</li>
+                    <li>Gi1 Languages</li>
+                    <li>Gi1 Kids Zone</li>
+                    <li>Gi1 Real Estate</li>
+                    <li>Gi1 Sports</li>
+                    <li>Gi1 Freelance</li>
+                    <li>Gi1 Comedy</li>
+                    <li>Gi1 News</li>
+                </ul>
+            </div>
+        </div>
+        <div class="card col-md-3 m-1">
+            <div class="card-header">
+                <h2>Extra Categories</h2>
+            </div>
+            <div class="card-body">
+                <ul>
+                    <li>Gi1 Sharing</li>
+                    <li>Gi1 Legal</li>
+                    <li>Gi1 Docs</li>
+                    <li>Gi1 Book Store</li>
+                    <li>Gi1 Reciepes</li>
+                    <li>Gi1 Caller</li>
+                    <li>Gi1 A.I.</li>
+                    <li>Gi1 App Locks</li>
+                    <li>Gi1 Multiverse</li>
                 </ul>
             </div>
         </div>
     </div>
-
-    <div class="blog-item">
-        <div class="blog-content">
-            <h2 class="blog-title">Main Industries</h2>
-
-        </div>
-    </div>
-
-    <div class="blog-item">
-        <img class="blog-image" src="images\ecommerce.jpeg" alt="Blog Image 3">
-        <div class="blog-content">
-            <h2 class="blog-title">Gi1 MALL</h2>
-        </div>
-    </div>
-</section>
+</div>
 
 
 <style>
@@ -532,96 +572,96 @@ include 'header.php'; ?>
 
                         </select> -->
                         <input type="checkbox" name="platform[]" value="Gi1 Media" id="gi1Media">
-    <label for="gi1Media">Gi1 Media</label>
+                        <label for="gi1Media">Gi1 Media</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Mall" id="gi1Mall">
-    <label for="gi1Mall">Gi1 Mall</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Mall" id="gi1Mall">
+                        <label for="gi1Mall">Gi1 Mall</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Video Hub" id="gi1VideoHub">
-    <label for="gi1VideoHub">Gi1 Video Hub</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Video Hub" id="gi1VideoHub">
+                        <label for="gi1VideoHub">Gi1 Video Hub</label>
 
-    <!-- Repeat for other checkboxes -->
-    
-    <input type="checkbox" name="platform[]" value="Gi1 Edits" id="gi1Edits">
-    <label for="gi1Edits">Gi1 Edits</label>
+                        <!-- Repeat for other checkboxes -->
 
-    <input type="checkbox" name="platform[]" value="Gi1 Game Zone" id="gi1GameZone">
-    <label for="gi1GameZone">Gi1 Game Zone</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Edits" id="gi1Edits">
+                        <label for="gi1Edits">Gi1 Edits</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Beauty" id="gi1Beauty">
-    <label for="gi1Beauty">Gi1 Beauty</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Game Zone" id="gi1GameZone">
+                        <label for="gi1GameZone">Gi1 Game Zone</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Travels" id="gi1Travels">
-    <label for="gi1Travels">Gi1 Travels</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Beauty" id="gi1Beauty">
+                        <label for="gi1Beauty">Gi1 Beauty</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Podcasts" id="gi1Podcasts">
-    <label for="gi1Podcasts">Gi1 Podcasts</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Travels" id="gi1Travels">
+                        <label for="gi1Travels">Gi1 Travels</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Classes" id="gi1Classes">
-    <label for="gi1Classes">Gi1 Classes</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Podcasts" id="gi1Podcasts">
+                        <label for="gi1Podcasts">Gi1 Podcasts</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Hiring" id="gi1Hiring">
-    <label for="gi1Hiring">Gi1 Hiring</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Classes" id="gi1Classes">
+                        <label for="gi1Classes">Gi1 Classes</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Health" id="gi1Health">
-    <label for="gi1Health">Gi1 Health</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Hiring" id="gi1Hiring">
+                        <label for="gi1Hiring">Gi1 Hiring</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Memes" id="gi1Memes">
-    <label for="gi1Memes">Gi1 Memes</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Health" id="gi1Health">
+                        <label for="gi1Health">Gi1 Health</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Trading" id="gi1Trading">
-    <label for="gi1Trading">Gi1 Trading</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Memes" id="gi1Memes">
+                        <label for="gi1Memes">Gi1 Memes</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Languages" id="gi1Languages">
-    <label for="gi1Languages">Gi1 Languages</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Trading" id="gi1Trading">
+                        <label for="gi1Trading">Gi1 Trading</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Kids Zone" id="gi1KidsZone">
-    <label for="gi1KidsZone">Gi1 Kids Zone</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Languages" id="gi1Languages">
+                        <label for="gi1Languages">Gi1 Languages</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Real Estate" id="gi1RealEstate">
-    <label for="gi1RealEstate">Gi1 Real Estate</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Kids Zone" id="gi1KidsZone">
+                        <label for="gi1KidsZone">Gi1 Kids Zone</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Sports" id="gi1Sports">
-    <label for="gi1Sports">Gi1 Sports</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Real Estate" id="gi1RealEstate">
+                        <label for="gi1RealEstate">Gi1 Real Estate</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Freelance" id="gi1Freelance">
-    <label for="gi1Freelance">Gi1 Freelance</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Sports" id="gi1Sports">
+                        <label for="gi1Sports">Gi1 Sports</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Comedy" id="gi1Comedy">
-    <label for="gi1Comedy">Gi1 Comedy</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Freelance" id="gi1Freelance">
+                        <label for="gi1Freelance">Gi1 Freelance</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 News" id="gi1News">
-    <label for="gi1News">Gi1 News</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Comedy" id="gi1Comedy">
+                        <label for="gi1Comedy">Gi1 Comedy</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Sharing" id="gi1Sharing">
-    <label for="gi1Sharing">Gi1 Sharing</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 News" id="gi1News">
+                        <label for="gi1News">Gi1 News</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Legal" id="gi1Legal">
-    <label for="gi1Legal">Gi1 Legal</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Sharing" id="gi1Sharing">
+                        <label for="gi1Sharing">Gi1 Sharing</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Docs" id="gi1Docs">
-    <label for="gi1Docs">Gi1 Docs</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Legal" id="gi1Legal">
+                        <label for="gi1Legal">Gi1 Legal</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Book Store" id="gi1BookStore">
-    <label for="gi1BookStore">Gi1 Book Store</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Docs" id="gi1Docs">
+                        <label for="gi1Docs">Gi1 Docs</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Recipes" id="gi1Recipes">
-    <label for="gi1Recipes">Gi1 Recipes</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Book Store" id="gi1BookStore">
+                        <label for="gi1BookStore">Gi1 Book Store</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Caller" id="gi1Caller">
-    <label for="gi1Caller">Gi1 Caller</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Recipes" id="gi1Recipes">
+                        <label for="gi1Recipes">Gi1 Recipes</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 A.I" id="gi1AI">
-    <label for="gi1AI">Gi1 A.I</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Caller" id="gi1Caller">
+                        <label for="gi1Caller">Gi1 Caller</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Notes" id="gi1Notes">
-    <label for="gi1Notes">Gi1 Notes</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 A.I" id="gi1AI">
+                        <label for="gi1AI">Gi1 A.I</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 App Locks" id="gi1AppLocks">
-    <label for="gi1AppLocks">Gi1 App Locks</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 Notes" id="gi1Notes">
+                        <label for="gi1Notes">Gi1 Notes</label>
 
-    <input type="checkbox" name="platform[]" value="Gi1 Multiverse" id="gi1Multiverse">
-    <label for="gi1Multiverse">Gi1 Multiverse</label>
+                        <input type="checkbox" name="platform[]" value="Gi1 App Locks" id="gi1AppLocks">
+                        <label for="gi1AppLocks">Gi1 App Locks</label>
+
+                        <input type="checkbox" name="platform[]" value="Gi1 Multiverse" id="gi1Multiverse">
+                        <label for="gi1Multiverse">Gi1 Multiverse</label>
 
                     </div>
                     <div class="col m-2">
