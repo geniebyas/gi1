820640b47f8857942ea8131e56701c2e70fdd9b2
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2023-11-30 12:41:32 +0530

    gi error

--- api/v1/get_industries.php
+++ api/v1/get_industries.php
@@ -5,28 +5,24 @@ try{
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $getIndustries = "SELECT * FROM industry WHERE status = true";
-    $result = mysqli_query($conn,$getIndustries);
+    $result = mysqli_query($con,$getIndustries);
     $industries = array();
     if($result->num_rows > 0){
     while($row = mysqli_fetch_assoc($result)){
         $industries[] = $row;
+        }
     }
-}
     if(sizeof($industries) > 0){
-        header('Content-Type: application/json');
         echo json_encode($industries);
     }else{
-        header('Content-Type: application/json');
         echo json_encode(array("response" => "No Industries Available","code" => 404));
     }
     
 }else{
-    header("Content-Type: application/json");
     echo json_encode(array("response" => "Invalid Request","code"=>400));
 
 }
 }catch(Exception $e){
-    header("Content-Type: application/json");
     echo json_encode(array("response" => $e->getMessage(),"code"=>$e->getCode()));
 }
 
