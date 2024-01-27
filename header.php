cb75ba3a6523a03145f11880dab7e5849792c1a1
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-01-27 23:57:19 +0530

    header icon replaced

--- header.php
+++ header.php
@@ -65,6 +65,12 @@
 	<link rel="stylesheet" href="plugins/aos/aos.css">
 	<link rel="stylesheet" type="text/css" href="styles.css">
 
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
 	<!-- CUSTOM CSS -->
 	<link href="css/style.css" rel="stylesheet">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
