315901a3cf9096c5d9734f633a4308fefed73682
krakendev01 <sahilpatel19189@gmail.com>
2024-02-26 11:01:22 +0530

    slash

--- index.php
+++ index.php
@@ -3,18 +3,16 @@ session_start();
 $_SESSION['current_session'] = "home";
 include 'header.php'; 
 
-
-include url('indexpages/promovideo.php');
+include 'indexpages/promovideo.php';
 include 'indexpages/introduction.php';
-include 'indexpages\shortservices.php';
-include 'indexpages\prototype.php';
-include 'indexpages\slide.php';
-include 'indexpages\market.php';
-include 'indexpages\invester.php';
-include 'indexpages\starts.php';
-include 'indexpages\careers.php';
-include 'indexpages\about.php';
-
+include 'indexpages/shortservices.php';
+include 'indexpages/prototype.php';
+include 'indexpages/slide.php';
+include 'indexpages/market.php';
+include 'indexpages/invester.php';
+include 'indexpages/starts.php';
+include 'indexpages/careers.php';
+include 'indexpages/about.php';
 
 include 'footer.php'; 
 
