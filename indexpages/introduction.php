d0d96ad95c8e29c07bc2fff137598b224c673bd4
krakendev01 <sahilpatel19189@gmail.com>
2024-03-06 10:39:22 +0530

    from github

--- indexpages/introduction.php
+++ indexpages/introduction.php
@@ -3,7 +3,7 @@
         <div class="row align-items-center">
             <div class="col-md-8">
                 <h2 style="color:black; text-align:center;" ><strong>All In One In Gi1</strong></h2>
-                <p style="color:black; text: size 30px;">'Gi1,' the world's 1<sup>st</sup> super app, embark on a revolutionary digital journey. Uniting top-tier platforms, it serves as the ultimate mall of apps, offering a comprehensive A-Z selection across industries and categories. Elevate your convenience and access all you need in a single, sophisticated app—'Gi1,' where innovation meets seamless functionality.
+                <p style="color:black; text: size:  50px;">'Gi1,' the world's 1<sup>st</sup> super app, embark on a revolutionary digital journey. Uniting top-tier platforms, it serves as the ultimate mall of apps, offering a comprehensive A-Z selection across industries and categories. Elevate your convenience and access all you need in a single, sophisticated app—'Gi1,' where innovation meets seamless functionality.
             </p>
             </div>
             <div class="col-md-4">
